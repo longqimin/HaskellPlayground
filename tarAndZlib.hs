cf7f76b0139093af1b1b27e93077c93777eb57bb
longqm <long2008920@163.com>
2014-07-11 16:10:42 +0800

    learning tar and zlib

---
+++ tarAndZlib.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Codec.Archive.Tar hiding (pack)
+import Codec.Compression.GZip
+import qualified Filesystem.Path as FP
+import Filesystem.Path.CurrentOS (encodeString, decodeString)
+import qualified Data.ByteString.Lazy as BL
+import System.Directory (getTemporaryDirectory, copyFile)
+
+tar :: FP.FilePath -> FP.FilePath -> IO ()
+tar src dest = do
+	let pathPieces = FP.splitDirectories src
+	let (base, fileOrDir) = if FP.filename src == FP.empty then (FP.concat $ init pathPieces, last pathPieces) 
+													 else (FP.directory src, FP.filename src) 
+	tmpDir <- getTemporaryDirectory
+	let tmpFile =(decodeString tmpDir) FP.</> dest
+	create (encodeString tmpFile) (encodeString base) $ map encodeString [fileOrDir]
+	copyFile (encodeString tmpFile) (encodeString dest)
+
+gzip :: FP.FilePath -> FP.FilePath -> IO ()
+gzip src dest = do
+	raw <- BL.readFile (encodeString src)
+	let compressed = compress raw 
+	BL.writeFile (encodeString dest) compressed
